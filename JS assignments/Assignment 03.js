e193ffc54c85386328e085fd7e0d6830080f8e71
drizzle0171 <yongsandew@gmail.com>
2022-03-18 21:52:50 +0900

    #3

---
+++ JS assignments/Assignment 03.js
@@ -0,0 +1,20 @@
+function operator(n1, operate, n2){
+    let answer;
+    switch(operate){
+        case '+':
+            answer = n1+n2;
+            break;
+        case '-':
+            answer = n1-n2;
+            break;            
+    }
+    return answer
+}
+
+function quest3(){
+    let num1 = parseInt(prompt("첫 번재 숫자를 입력하세요"));
+    let operate = prompt("연산자를 입력하세요");
+    let num2 = parseInt(prompt("두 번째 숫자를 입력하세요"));
+    answer = operator(num1, operate, num2);
+    console.log(`정답은 ${answer}입니다.`);
+}
