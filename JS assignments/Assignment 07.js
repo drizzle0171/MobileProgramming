7665aca1991a789e58d09caa5c815d0c6ac5fe69
drizzle0171 <yongsandew@gmail.com>
2022-03-19 22:25:01 +0900

    #7

---
+++ JS assignments/Assignment 07.js
@@ -0,0 +1,9 @@
+function quest7(){
+    let input = prompt("문자열을 입력해주세요.");
+    let string_list = input.split(' ');  
+    let result="";
+    for (var word=0; word<(string_list.length); word++){
+        result+=string_list[word][0].toUpperCase();
+    } 
+    console.log(result)
+}
