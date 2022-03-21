5ce04445afcfe5a68c34b31838d75c024814e9b0
drizzle0171 <yongsandew@gmail.com>
2022-03-21 17:19:10 +0900

    #9

--- JS assignments/Assignment 09.js
+++ JS assignments/Assignment 09.js
@@ -1,25 +1,24 @@
 function quest9(longStr){
-    let Str = longStr.replace(/ /g, ""); // 공백제거 완
-    let lowerCase = Str.toLowerCase().split(''); // split -> list
-    let new_arr = [...new Set(lowerCase)];
-
+    let Str = longStr.replace(/ |;|[.]/g, ""); // 공백제거 완
+    let Case = Str.split(''); // split -> list
+    let new_arr= [...new Set(Case)];
     let cnt_arr = [];
     let cnt = 0;
     for (let i=0; i<new_arr.length; i++){
-        for (let j=0; j<lowerCase.length; j++){
-            if (new_arr[i]==lowerCase[j]) {
+        for (let j=0; j<Case.length; j++){
+            if (new_arr[i]==Case[j]) {
                 cnt++;
             }
         }
         cnt_arr.push(cnt);
         cnt = 0;
     }
+
     let result = {};
     for (let k=0; k<cnt_arr.length; k++){
-        result.new_arr[k] = cnt_arr[k];
+        result[new_arr[k]] = cnt_arr[k];
     }
-    for (let key in result){
-        console.log(`${key}:${result[key]}`);
+    for (key in result){
+        console.log(`${key}:${result[key]}`)
     }
-    console.log(new_arr, cnt_arr);
 }
