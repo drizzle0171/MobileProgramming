f0ac10dac605de199bba328fa7e2720cc7d82e42
drizzle0171 <yongsandew@gmail.com>
2022-03-21 16:01:27 +0900

    #8

--- JS assignments/Assignment 08.js
+++ JS assignments/Assignment 08.js
@@ -1,13 +1,11 @@
 function quest8(){
     let input = prompt("문자열을 입력하세요.");
-    let arr = input.split(' ');
+    let words = input.replace(/,|[.]/gi, " ");
+    let arr = words.split(' ');
     let new_arr = [...new Set(arr)];;
     let list = new_arr.sort()
 
-    for (word of list){
-        if (word[-1]==(',' || '.')){
-            word = word.slice(0, -1);
+    for (let word=1; word<list.length; word++){
+        console.log(list[word])
         }
-        console.log(word);
-    }
 }
